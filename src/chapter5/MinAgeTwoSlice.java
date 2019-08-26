54b414e83928a2a798eafdf5baa5a69bc1d85553
leejinseong <dlwls5201@naver.com>
2019-08-27 02:27:05 +0900

    5-3 MinAgeTwoSlice 보류

---
+++ src/chapter5/MinAgeTwoSlice.java
@@ -0,0 +1,19 @@
+package chapter5;
+
+public class MinAgeTwoSlice {
+
+    public static void main(String[] args) {
+
+        int[] A = {4,2,2,5,1,5,8};
+
+        int result = solution(A);
+        System.out.println("result : " + result);
+    }
+
+    public static int solution(int[] A) {
+
+        int result = -1;
+
+        return result;
+    }
+}

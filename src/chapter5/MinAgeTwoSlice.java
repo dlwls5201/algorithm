7d784707dac7239eab2160766c05ffa4f35b3bc0
leejinseong <dlwls5201@naver.com>
2019-08-27 21:12:35 +0900

    5-3 MinAgeTwoSlice 풀이

--- src/chapter5/MinAgeTwoSlice.java
+++ src/chapter5/MinAgeTwoSlice.java
@@ -10,10 +10,38 @@ public class MinAgeTwoSlice {
         System.out.println("result : " + result);
     }
 
-    public static int solution(int[] A) {
+    //https://app.codility.com/demo/results/trainingY88WSW-KRJ/
+    private static int solution(int[] A) {
 
-        int result = -1;
+        float minTwoAvg = (A[0] + A[1]) / 2f;
+        int posTwoAvg = 0;
 
-        return result;
+        for(int i = 0; i < A.length - 1; i++) {
+            float tempTwoAge = (A[i] + A[i+1]) / 2f;
+
+            if(tempTwoAge < minTwoAvg) {
+                minTwoAvg = tempTwoAge;
+                posTwoAvg = i;
+            }
+        }
+
+        float minThreeAvg = 0;
+        int posThreeAvg = 0;
+
+        if(A.length > 2) {
+
+            minThreeAvg = (A[0] + A[1] + A[2]) / 3f;
+
+            for(int i = 0; i < A.length - 2; i++) {
+                float tempThreeAvg = (A[i] + A[i+1] + A[i+2]) / 3f;
+
+                if(tempThreeAvg < minThreeAvg) {
+                    minThreeAvg = tempThreeAvg;
+                    posThreeAvg = i;
+                }
+            }
+        }
+
+        return minTwoAvg < minThreeAvg ? posTwoAvg : posThreeAvg;
     }
 }

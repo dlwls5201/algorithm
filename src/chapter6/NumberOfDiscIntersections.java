7af08ab8c6b8598eca36754dbb676b44a23d513b
iwedding <dlwls5201@naver.com>
2019-09-03 19:20:46 +0900

    NumberOfDiscIntersectons 수정

--- src/chapter6/NumberOfDiscIntersections.java
+++ src/chapter6/NumberOfDiscIntersections.java
@@ -1,5 +1,7 @@
 package chapter6;
 
+import java.util.LinkedList;
+
 public class NumberOfDiscIntersections {
 
     public static void main(String[] args) {
@@ -19,8 +21,36 @@ public class NumberOfDiscIntersections {
         for(int i = 0; i < A.length - 1; i++) {
 
             for(int j = i + 1; j < A.length; j++) {
-                //i 원의 오른쪽 값이 j 원의 왼쪽 값보다 작거나 같으면 교점이 있는 것
+                //i 원의 오른쪽 값이 j 원의 왼쪽 값보다 크거나 같으면 교점이 있는 것
                 if(i + A[i] >= j - A[j]) result++;
+            }
+        }
+
+        return result;
+    }
+
+    //https://app.codility.com/demo/results/trainingK7RDFR-5FS/
+    //O(N^2)
+    private static int solution2(int[] A) {
+
+        int result = 0;
+
+        int[] rightValue = new int[A.length];
+        LinkedList<Integer> leftValue = new LinkedList<>();
+
+        for (int i = 0; i < A.length; i++) {
+            rightValue[i] = i + A[i];
+            leftValue.add(i - A[i]);
+        }
+
+        for (int j = 0; j < rightValue.length - 1; j++) {
+
+            leftValue.removeFirst();
+
+            for(int left : leftValue) {
+                if(rightValue[j] >= left) {
+                    result++;
+                }
             }
         }
 

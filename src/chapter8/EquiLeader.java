ba65b628c0a747a7c9fb1a8671e354aff20fbe4f
leejinseong <dlwls5201@naver.com>
2019-09-16 23:35:13 +0900

    chapter 8 EquiLeader

---
+++ src/chapter8/EquiLeader.java
@@ -0,0 +1,75 @@
+package chapter8;
+
+import java.util.HashMap;
+
+//https://app.codility.com/demo/results/training4ANHYB-DM4/
+public class EquiLeader {
+
+    /**
+     * 배열을 나눴을 때 지배자가 같은 경우의 수를 구하라
+     */
+    public static void main(String[] args) {
+
+        int[] A = {4, 3, 4, 4, 4, 2};
+
+        int result = solution(A);
+        System.out.println("result : " + result);
+
+    }
+
+    private static int solution(int[] A) {
+
+        int cases = 0;
+
+        for (int s = 0; s < A.length - 1; s++) {
+
+            int[] pre = new int[s + 1];
+            int[] post = new int[A.length - s - 1];
+
+            for (int i = 0; i < A.length; i++) {
+
+                if (i < s + 1) {
+                    pre[i] = A[i];
+                } else {
+                    post[i - s - 1] = A[i];
+                }
+
+            }
+
+            int preValue = dominator(pre);
+            int postValue = dominator(post);
+
+            if (postValue != -1 && preValue == postValue) cases++;
+        }
+
+        return cases;
+    }
+
+    private static int dominator(int[] A) {
+
+        int value = -1;
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < A.length; i++) {
+
+            if (hashMap.containsKey(A[i])) {
+                int cnt = hashMap.get(A[i]);
+
+                if (cnt >= A.length / 2) {
+                    value = A[i];
+                    break;
+                } else {
+                    hashMap.put(A[i], ++cnt);
+                }
+            } else {
+                hashMap.put(A[i], 1);
+
+                // A 값이 1개 일때만 예외 처리
+                if (A.length == 1) value = A[i];
+            }
+        }
+
+        return value;
+    }
+}

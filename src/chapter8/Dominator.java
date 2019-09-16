65330ded9bab8f2fb254223e4545096bfe0af8fc
leejinseong <dlwls5201@naver.com>
2019-09-16 22:56:30 +0900

    add description in ch8 dominator

--- src/chapter8/Dominator.java
+++ src/chapter8/Dominator.java
@@ -6,7 +6,13 @@ import java.util.HashMap;
 //TODO O(N*lonN) or N 인데 O(N*lonN)는 왜 나오는 거지?
 public class Dominator {
 
-    //A 배열 중에서 절반 이상을 차지하는 요소의 인덱스를 반환해라
+    /**
+     *
+     * A 배열 중에서 절반을 초과하는 요소의 인덱스를 반환해라
+     *
+     *  - 절반을 초과하는 지배자는 1개만 존재한다.
+     *  - 지배자를 나타내는 인덱스 중 아무거나 리턴하면 되므로 배열 절반을 초과하는 인덱스가 보이면 바로 리턴한다.
+     */
     public static void main(String[] args) {
 
         int[] A = {3, 4, 3, 2, 3, -1, 3, 3};
